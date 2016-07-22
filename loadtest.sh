4b46ec7782d7913e8d74dfaddec21bb9961959e0
Starduster <aria@starduster.me>
2016-07-22 20:25:23 +0800

    add comment

--- loadtest.sh
+++ loadtest.sh
@@ -9,7 +9,7 @@ CHECK_OK=0
 function check()
 {
     if echo $1 | egrep -q '^[0-9]+$'; then :
-    else echo -e "the $1 is not a valid input!\n" && CHECK_OK=1;
+    else echo -e "\nthe $1 is not a valid input!\n" && CHECK_OK=1;
     fi
 }
 
@@ -20,8 +20,11 @@ function run_test()
     echo -e "\nnow is $TIME \n"
     read -p "input the concurrency:" CON
     read -p "input the total requests:" REQ
-    read -p "http or https,1 is for http,2 is for https:" IF_HTTPS
-    
+    read -p "how many ab process do you want to run?" PROCESS_NUM
+    read -p "http or https,1 is for http,2 is for https,default is http:" IF_HTTPS
+    if [ "$IF_HTTPS" = "" ];then
+        IF_HTTPS=1
+    fi
     if [ "$IF_HTTPS" = "1" ];then
         PROTOCOL=http
     elif [ "$IF_HTTPS" = "2" ];then
@@ -29,20 +32,29 @@ function run_test()
     else    
         echo "input error" && exit 1
     fi
-    
-    read -p "input the lable of this test,e.g. haproxy:" TAG
-    read -p "how many ab process do you want to run?" PROCESS_NUM
-    
+
+#check input    
     for var in $CON $REQ $PROCESS_NUM;
     do
         check $var;
     done
-   
+
+#something else can be added to the log file name:
+    read -p "enable keep-alive connection? 1 is for yes,2 is for no,default is no:" IF_LONG
+    if [ "$IF_LONG" = "" ];then
+        IF_LONG=2
+    fi
+  
+    read -p "input the lable of this test,press enter for none:" TAG
+    if [ "$TAG" = "" ];then :
+    else TAG=$TAG-
+    fi
+
     if [ "$CHECK_OK" = "0"  ];then
         echo -e "testing...logging to $LOG_PATH/ \n"
         for (( i=1; i<$[PROCESS_NUM+1]; i=i+1 )); do
-            LOG_NAME=$LOG_PATH/$TAG-$TIME-$PROTOCOL-$REQ@$CON-$i.log
-            ab -r -n $REQ -c $CON $PROTOCOL://test.starduster.me/  > $LOG_NAME &  
+            LOG_NAME=$LOG_PATH/$PROTOCOL-$REQ@$CON-$TAG$TIME-$i-of-$PROCESS_NUM.log
+            ab -s 120 -r -n $REQ -c $CON $PROTOCOL://test.starduster.me/  > $LOG_NAME &  
             #tee - $LOG_NAME
         done
         wait
@@ -50,4 +62,5 @@ function run_test()
     fi
 }
 
+#entrance of this script:
 run_test
