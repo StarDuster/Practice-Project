33807a0b343deed7b9a6405ff92fd2c3914336ae
Starduster <aria@starduster.me>
2016-07-22 21:41:18 +0800

    edit watch.sh

--- watch.sh
+++ watch.sh
@@ -1,9 +1,16 @@
+#! /bin/bash
 clear
+export LANG=en_US
+export LC_ALL=en_US.UTF-8
+LOG_PATH=/root/log
+TIME=`date | cut -d " " -f 4`
+LOG_NAME=$LOG_PATH/connections-$TIME.log
+
 CON_NEW=$(netstat -s | grep 'active connections openings' | awk '{print $1}')
 COUNT=0
 until [ $COUNT -eq 3 ];
 do
-    echo -e "active connections:$CON_NEW, delta $[CON_NEW - CON]";
+    echo -e "active connections:$CON_NEW, delta $[CON_NEW - CON]" | tee -a $LOG_NAME;
     sleep 1;
     CON=$CON_NEW;
     CON_NEW=$(netstat -s | grep 'active connections openings' | awk '{print $1}')
@@ -15,6 +22,6 @@ do
     fi
 done 
 
-echo -e "connections lasts at $CON for 3s,exit"
+echo -e "connections lasts at $CON for 3s,exit\n"
 
 
