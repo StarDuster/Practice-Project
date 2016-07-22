945796ebe35f7ad29cbdcd46ab92717436e4b0ed
Starduster <aria@starduster.me>
2016-07-22 20:41:24 +0800

    make input checking better

--- loadtest.sh
+++ loadtest.sh
@@ -4,12 +4,12 @@ export LANG=en_US
 export LC_ALL=en_US.UTF-8
 LOG_PATH=/root/log
 
-CHECK_OK=0
+#CHECK_OK=0
 
 function check()
 {
     if echo $1 | egrep -q '^[0-9]+$'; then :
-    else echo -e "\nthe $1 is not a valid input!\n" && CHECK_OK=1;
+    else echo -e "\nThe input $1 is not a valid number!\n" && exit 1;  
     fi
 }
 
@@ -19,8 +19,17 @@ function run_test()
 
     echo -e "\nnow is $TIME \n"
     read -p "input the concurrency:" CON
+        check $CON;
     read -p "input the total requests:" REQ
+        check $REQ;
     read -p "how many ab process do you want to run?" PROCESS_NUM
+        check $PROCESS_NUM;
+
+#    for var in $CON $REQ $PROCESS_NUM;
+#    do
+#        check $var;
+#    done
+
     read -p "http or https,1 is for http,2 is for https,default is http:" IF_HTTPS
     if [ "$IF_HTTPS" = "" ];then
         IF_HTTPS=1
@@ -33,33 +42,29 @@ function run_test()
         echo "input error" && exit 1
     fi
 
-#check input    
-    for var in $CON $REQ $PROCESS_NUM;
-    do
-        check $var;
-    done
-
 #something else can be added to the log file name:
     read -p "enable keep-alive connection? 1 is for yes,2 is for no,default is no:" IF_LONG
     if [ "$IF_LONG" = "" ];then
         IF_LONG=2
     fi
-  
+    if [ "$IF_LONG" = "1" ];then
+        LONG="-k"
+    else
+        LONG=""
+    fi
+
     read -p "input the lable of this test,press enter for none:" TAG
     if [ "$TAG" = "" ];then :
     else TAG=$TAG-
     fi
 
-    if [ "$CHECK_OK" = "0"  ];then
-        echo -e "testing...logging to $LOG_PATH/ \n"
+    echo -e "testing...logging to $LOG_PATH/ \n"
         for (( i=1; i<$[PROCESS_NUM+1]; i=i+1 )); do
             LOG_NAME=$LOG_PATH/$PROTOCOL-$REQ@$CON-$TAG$TIME-$i-of-$PROCESS_NUM.log
-            ab -s 120 -r -n $REQ -c $CON $PROTOCOL://test.starduster.me/  > $LOG_NAME &  
+            ab -s 120 -r $LONG -n $REQ -c $CON $PROTOCOL://test.starduster.me/  > $LOG_NAME &  
             #tee - $LOG_NAME
         done
         wait
-    else echo -e "\nCheck fail!\n" && exit 1;
-    fi
 }
 
 #entrance of this script:
